09603f0b7601488f98d5e1c9d01ff65b42d5d258
DavidRobsonDev <david.dezanete.tavares@escola.pr.gov.br>
2025-03-18 12:31:01 -0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,102 @@
+/*
+let arr=[1,2,3]
+arr.push(4);
+console.log(arr)
+*/
+
+/*
+let arr=[1,2,3,4]
+let last=arr.pop()
+console.log(arr)
+*/
+
+/*
+let arr=[1,2,3,4]
+let first=arr.shift()
+console.log(arr)
+*/
+
+/*
+let arr=[2,3]
+arr.unshift(1)
+console.log(arr)
+*/
+
+/*
+let arr=[1,2,3,4,5]
+arr.splice(2,1)
+console.log(arr)
+*/
+
+/*
+let arrum=[1,2]
+let arrdois=[3,4]
+let combined=arrum.concat(arrdois)
+console.log(arr)
+*/
+
+/*
+let arr=[1,2,3]
+arr.forEach(item=>console.log(item))
+console.log(arr)
+*/
+
+/*
+let arr=[1,2,3]
+let dobro=arr.map(item=> item*2)
+console.log(arr)
+*/
+
+/*
+let array1=[1,2,3,4]
+array1.push(5)
+console.log(array1)
+*/
+
+/*
+let array2=[10,20,30,40]
+array2.pop()
+console.log(array2)
+*/
+
+/*
+let array3=[5,10,15,20]
+array3.shift()
+console.log(array3)
+*/
+
+/*
+let array4=[1,2,3]
+array4.unshift(0)
+console.log(array4)
+*/
+
+/*
+let array5=[10,20,30,40]
+array5.splice(2,1)
+console.log(array5)
+*/
+
+/*
+let array6=[10,20,30,40]
+array6.slice(1,3)
+console.log(array6)
+*/
+
+/*
+let array7=[1,2,3]
+let array8=[4,5,6]
+let array9=array7.concat(array8)
+console.log(array9)
+*/
+
+/*
+let array9=[2,4,6]
+array9.forEach(elemento=>console.log(elemento))
+*/
+
+/*
+let array=[2,4,6]
+let triplo=array.map(item=>item*3)
+console.log(triplo)
+*/
